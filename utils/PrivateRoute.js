a5a62ef15bdeb5753d855d22ed0b93186b189b39
sltsheryl <sltsheryl@gmail.com>
2023-06-27 15:01:26 +0800

    Fix refresh redirect bug

--- utils/PrivateRoute.js
+++ utils/PrivateRoute.js
@@ -1,25 +1,31 @@
-import { useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { useRouter } from "next/router";
 import { AuthContext } from "../contexts/Auth.Context";
 
 const OPENROUTES = ["/join", "/login", "/", "/register"];
 
 const PrivateRoute = ({ children }) => {
-  if (typeof window === "undefined") return children;
-
   const router = useRouter();
   const { auth } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // if (auth.loading === true) return (<h1>Loading...</h1>)
-  if (!auth.isAuthenticated && !OPENROUTES.includes(router.pathname))
-    router.push("/");
-
-  if (auth.isAuthenticated && router.pathname == "/") {
-    if (auth.userType === "teacher") {
-      router.push("/teacher");
-    } else {
-      router.push("/student/");
+  useEffect(() => {
+    if (!auth.loading) {
+      setIsLoading(false);
+      if (!auth.isAuthenticated && !OPENROUTES.includes(router.pathname))
+        router.push("/");
+      if (auth.isAuthenticated && router.pathname === "/") {
+        if (auth.userType === "teacher") {
+          router.push("/teacher");
+        } else {
+          router.push("/student");
+        }
+      }
     }
+  }, [auth.loading, auth.isAuthenticated, auth.userType, router.pathname]);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
   }
 
   return children;
